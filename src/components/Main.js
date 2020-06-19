6b30c6d8f2ae7ef9a2dbd373faa34f033eba15e9
Ting Kai Wu <tingkaiwuu@gmail.com>
2020-06-19 00:20:55 -0400

    add react-router and register component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,23 @@
-import React, {Component} from 'react';
-import {Register} from './Register';
+import React, { Component } from 'react';
+import { Register } from './Register';
+import { Switch, Router } from 'react-router-dom';
+import { Login } from './Login';
 
 class Main extends Component {
-   render() {
-       return (
-           <div className="main">
-               <Register />
-           </div>
-       );
-   }
+    render() {
+        return (
+            <div className="main">
+                <Switch>
+                    <Router path="/register">
+                        <Register />
+                    </Router>
+                    <Router path="/login">
+                        <Login />
+                    </Router>
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default Main;
